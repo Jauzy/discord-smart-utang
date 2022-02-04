0d418fee9a57597abdbb2506cede2dcc8bee84ee
Jauzy <al.zaujy@gmail.com>
2022-02-04 12:20:56 +0700

    update

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function printLogs(message){
     await Promise.all(utang_list.map( async utang => {
         let cmd = 'bayar'
         if(utang.total_amount > 0) cmd = 'utang'
-        list += `Transaction '${cmd.toUpperCase()}' from ${utang.utang_origin} to ${utang.utang_target} Rp. ${new Intl.NumberFormat().format(utang.dataValues.utang_amount)} on ${moment(utang.createdAt).locale('id').format('Do MMMM YYYY')}, issued by ${utang.utang_issuedby} on ${utang.utang_issuedchannel}\n`
+        list += `Transaction '${cmd.toUpperCase()}' from ${utang.utang_origin} to ${utang.utang_target} Rp. ${new Intl.NumberFormat().format(utang.dataValues.utang_amount)} on ${moment(utang.createdAt).locale('id').format('Do MMMM YYYY h:mm')}, issued by ${utang.utang_issuedby} on ${utang.utang_issuedchannel}\n`
     }))
     await message.reply(list)
 }
