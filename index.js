3f35b7e41d923c6a585cd5937862e3da13b959cb
Jauzy <al.zaujy@gmail.com>
2022-02-04 12:26:53 +0700

    update

--- index.js
+++ index.js
@@ -72,12 +72,13 @@ async function printReply(message, args){
 async function printLogs(message){
     let utang_list = await readAll()
     let list = '~ LOG UTANG ~\n'
+    await message.reply(list)
     await Promise.all(utang_list.map( async utang => {
         let cmd = 'bayar'
         if(utang.total_amount > 0) cmd = 'utang'
-        list += `Transaction '${cmd.toUpperCase()}' from ${utang.utang_origin} to ${utang.utang_target} Rp. ${new Intl.NumberFormat().format(utang.dataValues.utang_amount)} on ${moment(utang.createdAt).locale('id').format('Do MMMM YYYY h:mm')}, issued by ${utang.utang_issuedby} on ${utang.utang_issuedchannel}\n`
+        await message.reply(`Transaction '${cmd.toUpperCase()}' from ${utang.utang_origin} to ${utang.utang_target} Rp. ${new Intl.NumberFormat().format(utang.dataValues.utang_amount)} on ${moment(utang.createdAt).locale('id').format('Do MMMM YYYY h:mm')}, issued by ${utang.utang_issuedby} on ${utang.utang_issuedchannel}\n`)
     }))
-    await message.reply(list)
+    // await message.reply(list)
 }
 
 client.once('ready', () => {
