9984bab108334d3e09f9cd1da10f3375410f8ae1
Michael Addryan Liwe <michaeliwe98@gmail.com>
2022-08-18 21:08:59 +0700

    patch 18082022 add all command

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
 
 
 const prefix = process.env.PREFIX
+const kontrakanMembers = process.env.MEMBERS.split(',')
 const token = process.env.TOKEN.toString()
 
 const client = new Client({
@@ -81,6 +82,46 @@ async function printLogs(message){
     // await message.reply(list)
 }
 
+async function createUtang(message, origin, target, args) {
+    let origin_to_target = await readData(origin, target)
+    let target_to_origin = await readData(target, origin)
+    origin_to_target = origin_to_target?.[0]?.dataValues?.total_amount
+    target_to_origin = target_to_origin?.[0]?.dataValues?.total_amount 
+
+    if (args[1] == 'utang') {
+        if (target_to_origin > 0) {
+            if(target_to_origin > parseInt(args[3])){
+                await createData(message,target, origin, `-${args[3]}`)
+                await message.reply(`${origin} utang ${target} ${args[3]} berhasil ditambahkan`);
+            } else {
+                let utang_left = Math.abs(target_to_origin - parseInt(args[3]))
+                await createData(message,target, origin, `-${target_to_origin}`)
+                await createData(message,origin, target, `${utang_left}`)
+                await message.reply(`${origin} utang ${target} ${args[3]} berhasil ditambahkan`);
+            }
+        } else {
+            await createData(message,origin, target, args[3])
+            await message.reply(`${origin} utang ${target} ${args[3]} berhasil ditambahkan`);
+        }
+    } 
+    else if (args[1] == 'bayar') {
+        if(origin_to_target < parseInt(args[3])){
+            await createData(message,origin, target, `-${origin_to_target}`)
+            await createData(message,target, origin, parseInt(args[3]) - origin_to_target)
+            await message.reply(`utang ${args[3]} milik ${target} berhasil dibayar oleh ${origin}`);
+            printReply(message)
+        } 
+        else {
+            await createData(message,origin, target, `-${parseInt(args[3])}`)
+            await message.reply(`utang ${args[3]} milik ${target} berhasil dibayar oleh ${origin}`);
+            printReply(message)
+        }
+    }
+    else {
+        await message.reply('commandnya salah oi. menghadeh.')
+    }
+}
+
 client.once('ready', () => {
     console.log(token, prefix);
 });
@@ -88,7 +129,7 @@ client.once('ready', () => {
 client.on('message', async message => {
     const fromDiscord = message.content.toLowerCase();
     if (!fromDiscord.startsWith(prefix) || message.author.bot) return
-    const args = message.content.slice(prefix.length+1).split(/ +/)
+    let args = message.content.slice(prefix.length+1).split(/ +/)
 
     if(!args[0]) await message.reply('oi ngapain su, masukin commandnya lah. menghadeh')
     // else if(args[0] == 'logs'){
@@ -106,54 +147,34 @@ client.on('message', async message => {
         }
     } 
     else {
-        const listBadut = ['mike', 'joji', 'rery', 'imam', 'syafiq', 'akbar']
         if(!args[0] || !args[1] || !args[2] || !args[3])
             await message.reply('oi commandnya yang bener la. ex: oi joji utang imam 5000')
-        else if (!listBadut.includes(args[0]) || !listBadut.includes(args[2])) {
+        else if (!kontrakanMembers.includes(args[0]) || !kontrakanMembers.includes(args[2])) {
             await message.reply('oi ada yg namanya salah 🤡 (mike,joji,rery,imam,syafiq,akbar)')
         }
         else {
-            let origin_to_target = await readData(args[0], args[2])
-            let target_to_origin = await readData(args[2], args[0])
-            origin_to_target = origin_to_target?.[0]?.dataValues?.total_amount
-            target_to_origin = target_to_origin?.[0]?.dataValues?.total_amount 
-    
-            if (args[1] == 'utang') {
-                if (target_to_origin > 0) {
-                    if(target_to_origin > parseInt(args[3])){
-                        await createData(message,args[2], args[0], `-${args[3]}`)
-                        await message.reply(`utang berhasil ditambahkan`);
-                        printReply(message)
+            // all command handle
+            if (args[0] === 'all') { 
+                // divide the amount of utang to each kontrakanMember
+                args[3] = args[3]/(kontrakanMembers.length - 2)
+
+                for (const kontrakanMember of kontrakanMembers) {
+                    // skip all and target member
+                    if (['all', args[2]].includes(kontrakanMember)) {
+                        continue
                     } else {
-                        let utang_left = Math.abs(target_to_origin - parseInt(args[3]))
-                        await createData(message,args[2], args[0], `-${target_to_origin}`)
-                        await createData(message,args[0], args[2], `${utang_left}`)
-                        await message.reply(`utang berhasil ditambahkan`);
-                        printReply(message)
+                        await createUtang(message, kontrakanMember, args[2], args);
                     }
-                } else {
-                    await createData(message,args[0], args[2], args[3])
-                    await message.reply(`utang berhasil ditambahkan`);
-                    printReply(message)
-                }
-            } 
-            else if (args[1] == 'bayar') {
-                if(origin_to_target < parseInt(args[3])){
-                    await createData(message,args[0], args[2], `-${origin_to_target}`)
-                    await createData(message,args[2], args[0], parseInt(args[3]) - origin_to_target)
-                    await message.reply(`utang berhasil dibayar`);
-                    printReply(message)
-                } 
-                else {
-                    await createData(message,args[0], args[2], `-${parseInt(args[3])}`)
-                    await message.reply(`utang berhasil dibayar`);
-                    printReply(message)
                 }
             }
+            
+            // general command handle
             else {
-                await message.reply('commandnya salah oi. menghadeh.')
+                await createUtang(message, args[0], args[2], args);
             }
         }
+
+        await printReply(message)
     }
 })
 
